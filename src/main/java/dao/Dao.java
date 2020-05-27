5db8cc0706ad2e2f5ef5b2a4885ffa418930eb81
StaNC2000 <stanislaw.chynek@gmail.com>
2020-05-27 15:36:08 +0200

    Method connect() added

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -9,4 +9,25 @@ public abstract class Dao {
         protected Connection connection;
         PreparedStatement statement;
     }
+
+    private void connect() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection("jdbc:postgresql://ec2-54-75-246-118.eu-west-1.compute.amazonaws.com/" +
+                            database,
+                    login,
+                    password);
+
+            if (connection != null) {
+                System.out.println("Connected");
+            } else {
+                System.out.println("Connection Failed");
+            }
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+
 }
