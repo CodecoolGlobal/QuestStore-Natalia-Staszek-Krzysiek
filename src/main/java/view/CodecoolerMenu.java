fb5f318086aa9e94800a2ef07fe6f17b141cfef5
Krzysiek <chrisphilipniznik@gmail.com>
2020-05-28 14:32:46 +0200

    LOL

--- src/main/java/view/CodecoolerMenu.java
+++ src/main/java/view/CodecoolerMenu.java
@@ -1,4 +1,11 @@
 package view;
 
 public class CodecoolerMenu {
+
+    String[] options = {"See my wallet.","Buy an artifact.","Buy an artifact together with your team.","See my level of experience"};
+
+
+
 }
+
+
