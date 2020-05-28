44a3740d7a31cfe3b13bf3552325f5aec0949f26
NataliaRajska <natalia.rajskaa@gmail.com>
2020-05-28 18:17:54 +0200

    Add codecooler menu

--- src/main/java/controller/MentorController.java
+++ src/main/java/controller/MentorController.java
@@ -5,50 +5,6 @@ public class MentorController {
     public MentorController() {
     }
 
-    public void MenuMentor() throws Exception {
-        boolean isRunning = true;
-        while (isRunning) {
-            view.clearScreen();
-            view.displayMenu();
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            switch (input) {
-                case "1":
-                    createCodecooler();
-                    break;
-                case "2":
-                    addNewQuest();
-                    break;
-                case "3":
-                    addQuestToCategory();                    break;
-                case "4":
-                    editQuest();
-                    break;
-                case "5":
-                    markQuest();
-                    break;
-                case "6":
-                    addArtifactToStore();
-                    break;
-                case "7":
-                    editArtifact();
-                    break;
-                    addArtifactToCategory();
-                case "8":
-                    markArtifact();
-                    break;
-                case "9":
-                    showCodecollerWallet();
-                    break;
-                case "10":
-                    isRunning = false;
-                    break;
-                default:
-                    break;
-
-            }
-        }
-    }
     public void createCodecooler(){
 
     }
