b3ca22a0ac0b18e7f9529af24740972c6f13c2c5
Krzysiek <chrisphilipniznik@gmail.com>
2020-06-05 23:06:37 +0200

    Add ArtifacView and TeamView classes

---
+++ src/main/java/view/ArtifactView.java
@@ -0,0 +1,83 @@
+package view;
+
+import Utilities.ConsoleInputGetter;
+import model.Item;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.List;
+
+public class ArtifactView extends View {
+
+    public void displayCreatingItem() {
+        clearConsole();
+        System.out.println("Create new bonus menu");
+    }
+
+    public String displayGetName() {
+        return ConsoleInputGetter.getStringInputFromConsole("Enter new name: ");
+    }
+
+    public int displayGetPrice() {
+        return ConsoleInputGetter.getIntegerFromConsole("Enter new price");
+    }
+
+    public String displayGetDescription() {
+        return ConsoleInputGetter.getStringInputFromConsole("Enter new description: ");
+    }
+
+    public void displayArtifactHasBeenAdded() {
+        System.out.println("Artifact has been added successfully!");
+    }
+
+    public String askForItemCategory() throws InputMismatchException {
+        List<String> itemCategories = new ArrayList<>(Arrays.asList("Basic","Extra"));
+
+        String userCategoryChoose = "";
+
+        while (!itemCategories.contains(userCategoryChoose)) {
+            System.out.println("Choose category of bonus, type \'Basic\' or \'Extra\': ");
+            userCategoryChoose = scan.next();
+        }
+
+        return userCategoryChoose;
+    }
+
+    public int getIdOfItem() {
+        return ConsoleInputGetter.getIntegerFromConsole("\nEnter id of item to edit: ");
+    }
+
+    public int askForPropertyToEdit(Item item) {
+        clearConsole();
+        return ConsoleInputGetter.getIntegerFromConsole(item.toString() +
+                "\n\nWhat would you like to update?:" +
+                "\n1. Name" +
+                "\n2. Price" +
+                "\n3. Category" +
+                "\n4. Description");
+    }
+
+    public void displayNoItems() {
+        System.out.println("There is no items!");
+        pressAnyKeyToContinue();
+    }
+
+    public String getStudentLoginToMarkArtifact() {
+        return ConsoleInputGetter.getStringInputFromConsole("Enter student login to mark the artifact: ");
+    }
+
+    public String getItemNameInput() {
+        return ConsoleInputGetter.getStringInputFromConsole("Enter item name: ");
+    }
+
+    public void displayItemHasBeenMarked() {
+        System.out.println("Item has been marked!");
+        pressAnyKeyToContinue();
+    }
+
+    public void displayErrorMarkingItem() {
+        System.out.println("Error marking item!");
+        pressAnyKeyToContinue();
+    }
+}
---
+++ src/main/java/view/TeamView.java
@@ -0,0 +1,59 @@
+package view;
+
+public class TeamView extends View {
+
+    public String getTeamNameInput() {
+        System.out.println("Enter group name: ");
+        return getStringInput();
+    }
+
+    public void displayGroupAdded() {
+        System.out.println("Group has been added!");
+        pressAnyKeyToContinue();
+    }
+
+    public void displayGroupWithThisNameAlreadyExists() {
+        System.out.println("There is no group with this name!");
+        pressAnyKeyToContinue();
+    }
+
+    public void displayThereIsNoGroupWithThisName() {
+        System.out.println("There is no group with this name!");
+        pressAnyKeyToContinue();
+    }
+
+    public void displayGroupDeleted() {
+        System.out.println("Group has been deketed!");
+        pressAnyKeyToContinue();
+    }
+
+    public void displayGroupConnectionAdded() {
+        System.out.println("Group connection has been added!");
+        pressAnyKeyToContinue();
+    }
+
+    public void displayErrorAddingGroupConnection() {
+        System.out.println("Error adding a connection!");
+        pressAnyKeyToContinue();
+    }
+
+    public void displayGroupConnectionRemoved() {
+        System.out.println("Group connection has been removed!");
+        pressAnyKeyToContinue();
+    }
+
+    public void displayErrorRemovingGroupConnection() {
+        System.out.println("Error removing a group connection!");
+        pressAnyKeyToContinue();
+    }
+
+    public void displayGroupName(String groupName) {
+        System.out.println("");
+        System.out.println("Group name: " + groupName + " | Students in the group: ");
+    }
+
+    public void displayThisGroupHasNoStudentsAssigned() {
+        System.out.println("This group has no students assigned!");
+        pressAnyKeyToContinue();
+    }
+}
