ba872aeeb099c4370beeed49ee2c2fe65237edbd
Krzysiek <chrisphilipniznik@gmail.com>
2020-06-05 11:52:01 +0200

    Implement menthods for displaying and taking inputs from users in RootView

--- src/main/java/view/RootView.java
+++ src/main/java/view/RootView.java
@@ -1,4 +1,29 @@
 package view;
 
+import java.util.Scanner;
+
 public class RootView {
+
+    public void displayMenu() {
+        System.out.println("QUEST STORE" +
+                "\nAVAILABLE OPTIONS:" +
+                "1. LOGIN IN\n" +
+                "2. SIGN UP\n" +
+                "0. EXIT QUEST STORE");
+    }
+
+    public String getUserInput() {
+        System.out.print("Choose option: ");
+        return getStringInput();
+    }
+
+    private String getStringInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public void displayWrongInputMessage() {
+        System.out.println("WROND INPUT!");
+    }
+
 }
