70a19435a4cad4640bda7377fe6a7a9eb18b0644
Krzysiek <chrisphilipniznik@gmail.com>
2020-06-07 18:34:10 +0200

    Add StudentDetailsDao class in dao.SQL package

---
+++ src/main/java/dao/StudentDetailsDao.java
@@ -0,0 +1,15 @@
+package dao;
+
+import model.Codecooler;
+
+import java.util.List;
+
+public interface StudentDetailsDao {
+
+    Codecooler getStudentDataByStudentId(int student_id);
+    List<Codecooler> getAllStudentsData();
+    List<Codecooler> getStudentsDataByTeamName(String teamName);
+    boolean add(Codecooler student);
+    boolean updateStudentData(Codecooler student);
+    void setDatabasePath(String path);
+}
