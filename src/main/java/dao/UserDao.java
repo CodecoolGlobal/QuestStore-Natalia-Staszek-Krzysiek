ce68f184dc561fb25ba0414fec6f236bb47b26b2
Krzysiek <chrisphilipniznik@gmail.com>
2020-06-07 18:29:26 +0200

    Add UserDao class

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -2,13 +2,22 @@ package dao;
 
 import model.User;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public interface UserDao {
 
-    public ArrayList<User> extractor();
-
-    public void injector(User user);
-
+    List<User> getAll();
+    List<User> getAllByRole(String role);
+    List<User> getStudentsByGroupId(int id);
+    User getById(int id);
+    User getByLoginAndPassword(String login, String password);
+    User getByLoginAndRole(String login, String role);
+    User getByLogin(String login);
+    User getByEmail(String email);
+    User getByPhoneNumber(String phoneNumber);
+    boolean add(User user);
+    boolean update(User user);
+    boolean delete(User user);
+    void setDatabasePath(String path);
 
 }
