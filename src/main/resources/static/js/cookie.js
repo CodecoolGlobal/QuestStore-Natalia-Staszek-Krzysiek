d75acafabb72658e3eaf05edbb55018f818f249d
NataliaRajska <natalia.rajskaa@gmail.com>
2020-07-10 08:36:59 +0200

    Change var to const&let

--- src/main/resources/static/js/cookie.js
+++ src/main/resources/static/js/cookie.js
@@ -1,15 +1,15 @@
 function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
+  const d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  var expires = "expires="+d.toUTCString();
+  const expires = "expires="+d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
-  var cookienum = cname + "=";
-  var ca = document.cookie.split(';');
-  for(var i = 0; i < ca.length; i++) {
-    var c = ca[i];
+  const cookienum = cname + "=";
+  const ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
     while (c.charAt(0) == ' ') {
       c = c.substring(1);
     }
@@ -21,7 +21,7 @@ function getCookie(cname) {
 }
 
 function checkCookie() {
-  var cookienum = getCookie("username");
+  const cookienum = getCookie("username");
   if (cookienum != "") {
     alert("Welcome again " + cookienum);
   } else {
