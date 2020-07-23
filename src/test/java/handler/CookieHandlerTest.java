5f6bc26b514afd381bc475b21650d17ae9a828d9
MikeZielony <51535787+MikeZielony@users.noreply.github.com>
2020-07-23 08:33:20 +0200

    Add test CookieHandler

--- src/test/java/handler/CookieHandlerTest.java
+++ src/test/java/handler/CookieHandlerTest.java
@@ -1,10 +1,27 @@
 package handler;
 
+import com.sun.net.httpserver.HttpExchange;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+
 
 class CookieHandlerTest {
+
     CookieHandler cookieHandler = new CookieHandler();
 
-    @org.junit.jupiter.api.Test
-    void handle() {
+    @Test
+    void handle() throws IOException {
+
+        // Arrange
+        HttpExchange mock = Mockito.mock(HttpExchange.class);
+        Mockito.when(mock.getRequestHeaders().getFirst("Cookie")).thenReturn("");
+
+        //Act
+        cookieHandler.handle(mock);
+        // Assert
+        String cookie = Mockito.verify(mock.getRequestHeaders().getFirst("Cookie"), Mockito.times(1));
+        System.out.println(cookie);
     }
 }
